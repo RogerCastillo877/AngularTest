f3fa2b9351f07ced32649af82e90b829c2ffee5a
Roger Jamil Castillo Gomez <roger.castillo@globant.com>
2022-01-19 17:17:35 -0500

    Intermediate tests

---
+++ src/app/Intermediate/spies/medicos.component.spec.ts
@@ -0,0 +1,84 @@
+import { Observable, from, throwError, of } from 'rxjs';
+import { MedicosComponent } from './medicos.component';
+import { MedicosService } from './medicos.service';
+
+describe('MedicosComponent', () => {
+
+    let componente: MedicosComponent;
+    const servicio = new MedicosService(null as any);
+
+    beforeEach( () => {
+        componente = new MedicosComponent(servicio);
+    });
+
+
+    it('Init: debe cargar los médicos', () => {
+
+        const medicos = ['medico1', 'medico2', 'medico3'];
+
+        spyOn( servicio, 'getMedicos' ).and.callFake( () => {
+            return from( [ medicos ] );
+        })
+
+        componente.ngOnInit()
+
+        expect( componente.medicos.length ).toBeGreaterThan(0)
+    });
+
+    it('Debe llamar al servidor para agregar un médico', () => {
+        const espia = spyOn(servicio, 'agregarMedico').and.callFake( ()=> new Observable() );
+ 
+        componente.agregarMedico();
+ 
+        expect(espia).toHaveBeenCalled();
+    });
+    
+    it('debe agregar un médico al arreglo médicos', () => {
+      
+        const medico = { id: 1, nombre: 'Juan' };
+
+        spyOn( servicio, 'agregarMedico' )
+                .and.returnValue( from( [ medico ] ))
+        
+        componente.agregarMedico();
+
+        expect( componente.medicos.indexOf( medico ) ).toBeGreaterThanOrEqual(0);
+    });
+
+    it('Si falla la adición, la propiedad mensaje error, debe ser igual al error del servicio', () => {
+      
+        const miError = 'No se pudo agregar el médico';
+
+        spyOn( servicio, 'agregarMedico' ).and
+                .returnValue( throwError( miError ) );
+        
+        componente.agregarMedico();
+        
+        expect( componente.mensajeError ).toBe( miError );
+    });
+    
+    it('Debe de llamar al servidor para borrar un médico', () => {
+      
+        spyOn( window, 'confirm' ).and.returnValue(true);
+
+        const espia = spyOn( servicio, 'borrarMedico' )
+                            .and.returnValue( of() );
+
+        componente.borrarMedico('1');
+
+        expect( espia ).toHaveBeenCalledWith('1')
+    });
+
+    it('No debe de llamar al servidor para borrar un médico', () => {
+      
+        spyOn( window, 'confirm' ).and.returnValue(false);
+
+        const espia = spyOn( servicio, 'borrarMedico' )
+                            .and.returnValue( of() );
+
+        componente.borrarMedico('1');
+
+        expect( espia ).not.toHaveBeenCalledWith('1')
+    });
+    
+});
